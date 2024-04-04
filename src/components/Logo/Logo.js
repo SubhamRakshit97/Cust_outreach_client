16c9877e8ee176965b78dd3f45943212b389fec2
aneeta9843 <aneetakhanal9843@gmail.com>
2024-04-05 02:26:41 +0530

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+// material-ui
+/**
+ *
+ * import logoDark from 'assets/images/logo-dark.svg';
+ * import logo from 'assets/images/logo.svg';
+ *
+ */
+
+// ==============================|| LOGO ||============================== //
+import React from "react";
+import logo from '../../assets/images/techis-logo.png'
+const Logo = () => {
+
+  return (
+
+    <>
+      <img src={logo} alt="Tech IS" width="50" />
+     
+    </>
+  );
+};
+
+export default Logo;
